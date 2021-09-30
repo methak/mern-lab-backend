5938974de5fed8c793bc84a477a82c3ff74bd399
DaeyoungH <style6513@gmail.com>
2021-09-29 22:45:21 -0700

    completed server.js

--- server.js
+++ server.js
@@ -2,10 +2,26 @@
 require('./db/db')
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('./db/db')
 const PORT = 9000
 
 const wishlistController = require('./controllers/wishlist')
+const whiteList = ["http://localhost:3000"]
+const corsOptions = {
+  origin: (origin, callback) => {
+    if(whiteList.indexOf(origin) !== -1 || !origin ){
+      callback(null, true);
+    }else {
+      callback(new Error("Not allowed by Cors"))
+    }
+  }
+}
+
+// Middleware
 app.use('/wishlist', wishlistController)
+app.use(cors(corsOptions))
+app.use(express.json())
 
 app.listen(PORT, () => {
   console.log(`MERN LAB is running on port ${PORT}`)
