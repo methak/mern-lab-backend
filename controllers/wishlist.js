a586b6e3ee78cfdc6dd3d104566d15b4e535c82f
Dae Young Hwang <daeh@Daes-Air.attlocal.net>
2021-09-29 23:17:09 -0700

    set controller with CRUD & checked browser with empty arr

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -1,8 +1,50 @@
 const express = require('express')
 const router = express.Router()
+const WishList = require('../models/wishlist')
 
-router.get('/', (req, res) => {
-  res.send('Hello World')
+router.get('/', async (req, res) => {
+  try {
+    const allWishLists = await WishList.find()
+    res.status(200).json(allWishLists)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.get('/:id', async (res, req) => {
+  try {
+    const foundWishList = await WishList.findById(req.params.id);
+    res.status(200).json({ message: req.params.id })
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const newWishList = await WishList.create(req.body)
+    res.status(200).json(newWishList)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedWishList = await WishList.findByIDAndRemove(req.params.id)
+    res.status(200).json(deletedWishList)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedWishList = await WishList.findbyIdAndUpdate(req.params.id, req.body, {new:true})
+    res.status(200).json(updatedWishList)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
