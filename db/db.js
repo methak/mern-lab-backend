a84bae280af503d9a93f3f1010cc34e421d9aa6a
metha <matt.ksmp@gmail.com>
2021-10-02 23:19:35 -0700

    update code for deploy on Heroku

--- db/db.js
+++ db/db.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose')
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
-
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/whishlist'
 // Connection string (we will be replacing this later with environmental variables)
-mongoose.connect('mongodb://localhost:27017/whishlist', {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require('./db/db')
 const express = require('express')
 const app = express()
+const methodOverride  = require('method-override')
 const cors = require('cors')
 const PORT = process.env.PORT || 9000
 
@@ -20,6 +21,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(express.json())
+//use method override
+app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 app.use('/wishlist', wishlistController)
 
 
